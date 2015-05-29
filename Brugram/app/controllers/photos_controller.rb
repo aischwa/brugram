2b85950833bf55d53310f6aff9a91e74ef59f41a
Yining <sophietaoyn@gmail.com>
2015-05-29 20:05:45 +0200

    photo

---
+++ Brugram/app/controllers/photos_controller.rb
@@ -0,0 +1,46 @@
+class PhotosController < ActionController::Base
+	def index
+		@photos = Photo.all
+		end
+
+		def new
+			@photo = Photo.new
+		end
+
+		def show
+			@photo = Photo.find(params[:id])
+		end
+
+		def edit
+			@photo =Photo.find(params[:id])
+		end
+
+		def create
+			@photo = Photo.new(photo_params)
+			if @photo.save
+				redirect_to @place
+			else
+				render :new
+			end
+		end
+
+		def update
+			@photo = Place.find(params[:id])
+			if @place.update(photo_params)
+				redirect_to @photo
+			else
+				render 'edit'
+			end
+		end
+
+		def destroy
+			@photo = Photo.find(params[:id])
+			@photo.destroy
+			redirect_to photos_path
+		end
+
+		private 
+		def photo_params
+			params.requrie(:photo).permit(:caption, :image)
+		end
+	end
